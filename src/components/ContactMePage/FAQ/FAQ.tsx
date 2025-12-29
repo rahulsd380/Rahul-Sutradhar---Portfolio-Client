26b048379ba49b4119a41e76c0070a2d5acd701c
rahulsd380 <rahulsd380@gmail.com>
2025-12-29 16:36:06 +0600

    Made contact me section responsive

--- src/components/ContactMePage/FAQ/FAQ.tsx
+++ src/components/ContactMePage/FAQ/FAQ.tsx
@@ -43,7 +43,7 @@ const FAQ = () => {
 
   return (
     <Container>
-      <div className="pt-[150px] pb-[100px]">
+      <div className="pt-[100px] lg:pt-[150px] pb-[100px]">
         <Heading
           subHeading="FAQ"
           styledHeading="Frequently"
@@ -51,7 +51,7 @@ const FAQ = () => {
           description="Have questions? You’re not alone. Here are answers to common inquiries. If you need more help, reach out!"
         />
 
-        <div className="bg-neutral-85 border border-neutral-35/20 p-12 rounded-xl font-Inter mt-13 lg:mt-[70px] relative">
+        <div className="bg-neutral-85 border border-neutral-35/20 p-5 md:p-8 lg:p-12 rounded-xl font-Inter mt-13 lg:mt-[70px] relative">
           <div className="w-full h-full bg-primary-30/60 blur-[100px] absolute top-0 right-0 left-0 bottom-0"></div>
           <div className="flex gap-5 flex-col w-full relative">
             {accordingData?.map((according, index) => (
@@ -63,7 +63,7 @@ const FAQ = () => {
                   className="flex gap-2 cursor-pointer items-center justify-between w-full"
                   onClick={() => handleBorderClick(index)}
                 >
-                  <h2 className="text-white font-semibold">
+                  <h2 className="text-white font-semibold text-sm md:text-base">
                     {according.title}
                   </h2>
                   <p>
@@ -82,7 +82,7 @@ const FAQ = () => {
                       : "grid-rows-[0fr] opacity-0"
                   }`}
                 >
-                  <p className="text-neutral-35 overflow-hidden">
+                  <p className="text-neutral-35 overflow-hidden text-xs md:text-base">
                     {according.description}
                   </p>
                 </div>
